15904baa63426b7c90d414645bf6cb229ffbf199
DumitruIonutRazvan <dumitruionutrazvan92@gmail.com>
2023-10-25 17:31:32 +0300

    second try with an ng module component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputListComponent } from './input-list/input-list.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, InputListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/input-list/input-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'input-list',
+  templateUrl: './input-list.component.html',
+  styleUrls: ['./input-list.component.css']
+})
+export class InputListComponent {
+
+}
